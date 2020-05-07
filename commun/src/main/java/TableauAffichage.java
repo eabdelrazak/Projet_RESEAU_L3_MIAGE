eaa6e48688ff0e381bcbb3c524e8d1f6b532e103
JBLOGNON <jean-baptiste.lognon@etu.unice.fr>
2020-05-07 17:59:24 +0200

    Classe TableauAffichage

--- commun/src/main/java/TableauAffichage.java
+++ commun/src/main/java/TableauAffichage.java
@@ -1,2 +1,56 @@
+import java.util.Random;
+
 public class TableauAffichage {
+
+    private char[] enigmeADeviner;
+    private char[] enigmeDeviner;
+    private int longueurEnigme = 0;
+    private Random rand;
+
+    public TableauAffichage(){
+        this.rand = new Random();
+    }
+
+    //Methode pour definir l'énigme qui apparais sur le tableau
+    public void setEnigmeADeviner(String phrase){
+        this.longueurEnigme = phrase.length();
+        this.enigmeADeviner = new char[longueurEnigme];
+        this.enigmeDeviner = new char[longueurEnigme];
+
+        for(int i = 0; i < this.longueurEnigme; i++){
+            this.enigmeADeviner[i] = phrase.charAt(i);
+        }
+
+        for(int i = 0; i < this.longueurEnigme; i++){
+            this.enigmeDeviner[i] = ' ';
+        }
+    }
+
+    //Methode pour savoir combiend fois une lettre est presente dans l'énigme a deviné
+    public int chercherLettre(char lettre){
+        int nombreTrouver = 0;
+        for(int i = 0; i < this.longueurEnigme; i++){
+            if(this.enigmeADeviner[i] == lettre){
+                this.enigmeDeviner[i] = lettre;
+                nombreTrouver++;
+                this.enigmeADeviner[i] = ' ';
+            }
+        }
+        return nombreTrouver;
+    }
+
+    //Methode pour révéler une lettre aux hasard (a appeler toute les X secondes)
+    public void revelerLettre(){
+        boolean lettreReveler = false;
+
+        while(lettreReveler == false){
+            int nombreRandom = rand.nextInt(this.enigmeADeviner.length);
+            if(this.enigmeADeviner[nombreRandom] != ' '){
+                this.enigmeDeviner[nombreRandom] = this.enigmeADeviner[nombreRandom];
+                this.enigmeADeviner[nombreRandom] = ' ';
+                lettreReveler = true;
+            }
+        }
+    }
+
 }
