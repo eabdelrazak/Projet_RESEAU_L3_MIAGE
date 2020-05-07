508e384f19d3ca4774b513de64bf255b53e9895c
JBLOGNON <jean-baptiste.lognon@etu.unice.fr>
2020-05-07 11:14:12 +0200

    MAJ Roue

--- commun/src/main/java/Roue.java
+++ commun/src/main/java/Roue.java
@@ -1,17 +1,22 @@
+import java.util.Random;
+
 public class Roue {
 
     private String[] bonus;
+    private Random rand;
 
     public Roue(){
+        this.rand = new Random();
         initialisationRoue();
     }
 
     public String tourner(){
-        return "";
+        int result = rand.nextInt(this.bonus.length);
+        return this.bonus[result];
     }
 
     public void initialisationRoue(){
-
+        this.bonus = new String[]{"0", "150", "250", "300", "150", "200", "Banqueroute", "100", "300", "250", "100", "1000", "150", "250", "500", "Banqueroute", "1500", "150", "250", "Passe", "400", "2000", "100"};
     }
 
 }
