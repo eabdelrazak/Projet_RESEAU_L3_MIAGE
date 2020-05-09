24d63a3df6246b8f6ff621e52c39a1865de156c6
eabdelrazak <43001225+eabdelrazak@users.noreply.github.com>
2020-05-09 19:44:22 +0200

    Implementation des messages de communications

---
+++ commun/src/main/java/rouefortune/Messages.java
@@ -0,0 +1,12 @@
+package rouefortune;
+
+public class Messages {
+    /** MESSAGES SERVEUR → CLIENT **/
+    public static final String ENIGME_RAPIDE="Le serveur envoi un message pour afficher l'enigme rapide";
+    public static final String ENIGME_NORMAL="Le serveur envoi un message pour afficher l'enigme normal";
+
+    /** MESSAGE CLIENT → SERVEUR **/
+    public static final String BUZZ="Le client envoi un message pour indiquer qu'il a buzz";
+    public static final String TOURNER_ROUE="Le client envoi un message au serveur pour tourner la roue";
+    public static final String PROPOSER_LETTRE="Le client envoi un message pour proposer une lettre";
+}
